890586f9fdf63248e6c403905ca0cc9ff683b46c
Krisko-2503 <krisiangelov2009@gmail.com>
2025-09-29 12:30:42 +0300

    Add files via upload

---
+++ v00.02_ReleasedBeta_KinectLunnar/01.KinectLunnar/VisualHost.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace KinectLunnar
+{
+    public class VisualHost : FrameworkElement
+    {
+        private readonly VisualCollection _children;
+        public DrawingVisual Visual { get; }
+
+        public VisualHost()
+        {
+            _children = new VisualCollection(this);
+            Visual = new DrawingVisual();
+            _children.Add(Visual);
+        }
+
+        protected override int VisualChildrenCount => _children.Count;
+        protected override Visual GetVisualChild(int index) => _children[index];
+    }
+}
